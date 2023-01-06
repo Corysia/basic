6b289c591d1bc7deee1c80b4260ad1c3023ab273
Corysia Taware <corysia.taware@gmail.com>
2023-01-05 18:22:12 -0800

    create the canvas

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <HelloWorld/>
 </template>
 
 <script lang="ts">
@@ -16,12 +15,10 @@ export default defineComponent({
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+head, body, div, canvas {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  overflow: hidden;
 }
 </style>
